8bbac60badec6c396aa06b371b62ab2d4f290c76
Keima Senpai <98184310+KeimaSenpai@users.noreply.github.com>
2024-07-13 16:55:57 -0400

    Update peliculas.py

--- bot/peliculas.py
+++ bot/peliculas.py
@@ -11,7 +11,7 @@ def facts_to_str(user_data: Dict[str, str]):
             f'📽️ <b>Resolución:</b> {user_data["resolucion"]}\n'
             f'🔎 <b>Subtitulos:</b> {user_data["subs"]}\n'
             f'🎥 <b>Audio:</b> {user_data["audio"]}\n\n'
-            f'📝 <b>Sinopsis:</b> {user_data["sinopsis"]}\n\n\n'
+            f'📝 <b>Sinopsis:</b> <blockquote expandable>{user_data["sinopsis"]}</blockquote>\n\n\n'
             f'🔮 <b>Género:</b> {user_data["genero"]}\n'
             # f' <b>Subido por:</b> <a href="tg://user?id={user}">{name}</a>\n'
             '⚝━━━━━━━━━━━━━━━━━━━⚝\n'
